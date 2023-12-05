9e441fea95207e5ec44beac48086be031c660823
Rene V lepine <justingopijg@gmail.com>
2023-12-05 11:47:51 +0530

    Navabar component

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import CustomerReview from "./CustomerReview";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+export {
+    Hero,
+    PopularProducts,
+    CustomerReview,
+    Footer,
+    SuperQuality,
+    Subscribe,
+    Services,
+    SpecialOffer
+    ,
+
+}
